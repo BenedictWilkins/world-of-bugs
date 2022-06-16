10669b6b206c32e82fb08325a245a92a609f90d5
Benedict Wilkins <benrjw@gmail.com>
2022-06-16 17:25:48 +0100

    astyle

--- test/astyle.sh
+++ test/astyle.sh
@@ -1,10 +1,6 @@
-
-
-echo "Async?"
-
-#ASTYLE=$(which astyle)
-#if [ $? -ne 0 ]; then
-#	echo "astyle not installed, it can be installed with sudo apt-get install astyle or downloaded from http://astyle.sourceforge.net/"
-#	exit 1
-#fi
-#astyle ../com.worldofbugs.worldofbugs/*.cs --dry-run --exclude=PackageCache --recursive --style=attach -s4 -f -p -xg -U -xe -j -xp -xC88 -xL -xe --verbose -N
+ASTYLE=$(which astyle)
+if [ $? -ne 0 ]; then
+	echo "astyle not installed, it can be installed with sudo apt-get install astyle or downloaded from http://astyle.sourceforge.net/"
+	exit 1
+fi
+astyle com.worldofbugs.worldofbugs/*.cs --dry-run --exclude=PackageCache --recursive --style=attach -s4 -f -p -xg -U -xe -j -xp -xC88 -xL -xe --verbose -N
